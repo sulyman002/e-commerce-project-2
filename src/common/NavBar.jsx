829e8cb424dec76bcc9c50e98688950a17fd57ad
sulyman002 <oyedelesulaiman@gmail.com>
2025-09-04 18:01:44 +0100

    the cart section is done

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -8,6 +8,12 @@ import HeroSection from "../layouts/HeroSection";
 const NavBar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const location = useLocation();
+  const [cartDisplay, setCartdisplay] = useState(false);
+
+  function handleCartClicked() {
+    setCartdisplay(!cartDisplay);
+    console.log("cart clicked");
+  }
 
   const showHero = location.pathname === "/";
 
@@ -16,7 +22,7 @@ const NavBar = () => {
   }
   return (
     <div className="bg-[#101010] text-white ">
-      <div className="container mx-auto">
+      <div className="relative container mx-auto">
         <nav className=" border-b border-white/20 flex items-center justify-between px-6 p-4">
           <div className="flex items-center gap-8">
             <div onClick={handleNav} className="cursor-pointer md:hidden">
@@ -41,7 +47,7 @@ const NavBar = () => {
             </Link>
           </div>
 
-          <div>
+          <div onClick={handleCartClicked}>
             <img src={cart} alt="cart" />
           </div>
         </nav>
@@ -52,8 +58,8 @@ const NavBar = () => {
             isNavOpen ? "translate-x-0" : "-translate-x-full"
           }`}
         >
-          <div onClick={handleNav} className="flex flex-col justify-end p-4 mt-2">
-            <button className="">
+          <div onClick={handleNav} className="flex flex-col p-4 mt-2">
+            <button className="flex justify-end">
               <IoMdClose className="h-6 w-6 text-white font-bold" />
             </button>
 
@@ -84,6 +90,77 @@ const NavBar = () => {
       </div>
       {/* Show Hero Section */}
       <div className="container mx-auto">{showHero && <HeroSection />}</div>
+
+      {/* Cart container */}
+
+      {cartDisplay && (
+        <div className="fixed bg-black/60 inset-0 z-10">
+          <div className="flex justify-center md:justify-end md:items-start md:mt-20 md:mr-40 items-center h-full px-4">
+            <div className="w-3/4 md:w-[377px] h-[488px] bg-white rounded-lg">
+              <button
+                onClick={handleCartClicked}
+                className="flex justify-end w-full p-4 cursor-pointer"
+              >
+                <IoMdClose className="h-8 w-8 font-bold text-black" />
+              </button>
+              <div>
+                <div className="flex justify-between items-center px-6">
+                  <h2 className="text-[18px] font-bold text-black ">
+                    Cart (3)
+                  </h2>
+                  <div className="underline text-black/70 text-[15px] cursor-pointer ">
+                    Remove all
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex text-black flex-col gap-4 mt-3 h-1/2 overflow-y-auto bg-gray-100 px-6 mx-6 rounded-md">
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+                <p>game</p>
+              </div>
+              <div className="py-4 px-6 flex justify-between items-center">
+                <h2 className="text-[15px] font-bold text-black/50 ">Total</h2>
+                <h2 className="text-[20px] font-bold text-black ">$5,000</h2>
+              </div>
+              <div className="px-6 items-center w-full">
+                <button className="bg-[#D87D4A] w-full text-white p-4 hover:bg-orange-600 mb-4">
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* <div  
+        
+        className={`${cartDisplay ? "hidden " : " hidden"} fixed top-0 left-0`}
+      >
+        <nav>
+          <ul className="flex flex-col space-y-6 text-sm font-700 md:items-center mt-10 px-6">
+            <Link to="/" className=" hover:text-[#D87D4A] uppercase">
+              Home
+            </Link>
+            <Link to="headphones" className=" hover:text-[#D87D4A] uppercase">
+              Headphones
+            </Link>
+            <Link to="speakers" className=" hover:text-[#D87D4A] uppercase">
+              Speaker
+            </Link>
+            <Link to="earphones" className=" hover:text-[#D87D4A] uppercase">
+              Earphones
+            </Link>
+          </ul>
+        </nav>
+      </div> */}
     </div>
   );
 };
