88f20f4333ea59f3c58549719562fa5307012ce5
sulyman002 <oyedelesulaiman@gmail.com>
2025-09-04 18:51:11 +0100

    the cart content is set

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -4,11 +4,22 @@ import hamburger from "../assets/desktop_home/mobile/hamburger.svg";
 import cart from "../assets/desktop_home/mobile/cart.svg";
 import { IoMdClose } from "react-icons/io"; // Import the close icon from react-icons
 import HeroSection from "../layouts/HeroSection";
+import { useEffect } from "react";
 
 const NavBar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const location = useLocation();
   const [cartDisplay, setCartdisplay] = useState(false);
+  const [cartData, setCartData] = useState(null);
+
+  useEffect(() => {
+    fetch("/public/earphones.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCartData(data);
+        console.log(data?.[0].id);
+      });
+  }, []);
 
   function handleCartClicked() {
     setCartdisplay(!cartDisplay);
@@ -19,6 +30,17 @@ const NavBar = () => {
 
   function handleNav() {
     setIsNavOpen(!isNavOpen);
+  }
+
+  const [count, setCount] = useState(1);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function handleDecrement() {
+    if (count > 1) {
+      setCount(count - 1);
+    }
   }
   return (
     <div className="bg-[#101010] text-white ">
@@ -96,7 +118,7 @@ const NavBar = () => {
       {cartDisplay && (
         <div className="fixed bg-black/60 inset-0 z-10">
           <div className="flex justify-center md:justify-end md:items-start md:mt-20 md:mr-40 items-center h-full px-4">
-            <div className="w-3/4 md:w-[377px] h-[488px] bg-white rounded-lg">
+            <div className="w-4/5 md:w-[377px] h-[488px] bg-white rounded-lg">
               <button
                 onClick={handleCartClicked}
                 className="flex justify-end w-full p-4 cursor-pointer"
@@ -114,18 +136,128 @@ const NavBar = () => {
                 </div>
               </div>
 
-              <div className="flex text-black flex-col gap-4 mt-3 h-1/2 overflow-y-auto bg-gray-100 px-6 mx-6 rounded-md">
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
-                <p>game</p>
+              <div className="flex text-black flex-col gap-4 mt-3 h-1/2 overflow-y-auto bg-gray-200 py-3 px-3 mx-6 rounded-md">
+               
+              {/* cart content */}
+               <div className="flex items-center justify-between ">
+                  <div className="flex gap-2">
+                    <div className="h-[64px] w-[64px] bg-[#F1F1F1] rounded flex items-center justify-center ">
+                      <img
+                        src={cartData?.[0].image}
+                        alt=""
+                        className=" h-[40px] rounded "
+                      />
+                    </div>
+                    <div className="flex flex-col items-start justify-start gap-[2px] w-[76px] overflow-x-auto">
+                      <h2 className="text-[15px] font-bold whitespace-nowrap ">
+                        {cartData?.[0].title}
+                      </h2>
+                      <p className="text-sm font-bold text-black/50 ">
+                        $ {cartData?.[0].price}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex font-bold text-sm py-4 items-center justify-evenly gap-4 w-[120px] bg-[#F1F1F1] ">
+                    <div onClick={handleDecrement} className="text-black/70">
+                      -
+                    </div>
+                    <div className="">{count}</div>
+                    <div onClick={handleIncrement} className="text-black/70">
+                      +
+                    </div>
+                  </div>
+                </div>
+
+                 <div className="flex items-center justify-between ">
+                  <div className="flex gap-2">
+                    <div className="h-[64px] w-[64px] bg-[#F1F1F1] rounded flex items-center justify-center ">
+                      <img
+                        src={cartData?.[0].image}
+                        alt=""
+                        className=" h-[40px] rounded "
+                      />
+                    </div>
+                    <div className="flex flex-col items-start justify-start gap-[2px] w-[76px] overflow-x-auto">
+                      <h2 className="text-[15px] font-bold whitespace-nowrap ">
+                        {cartData?.[0].title}
+                      </h2>
+                      <p className="text-sm font-bold text-black/50 ">
+                        $ {cartData?.[0].price}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex font-bold text-sm py-4 items-center justify-evenly gap-4 w-[120px] bg-[#F1F1F1] ">
+                    <div onClick={handleDecrement} className="text-black/70">
+                      -
+                    </div>
+                    <div className="">{count}</div>
+                    <div onClick={handleIncrement} className="text-black/70">
+                      +
+                    </div>
+                  </div>
+                </div>
+
+                 <div className="flex items-center justify-between ">
+                  <div className="flex gap-2">
+                    <div className="h-[64px] w-[64px] bg-[#F1F1F1] rounded flex items-center justify-center ">
+                      <img
+                        src={cartData?.[0].image}
+                        alt=""
+                        className=" h-[40px] rounded "
+                      />
+                    </div>
+                    <div className="flex flex-col items-start justify-start gap-[2px] w-[76px] overflow-x-auto">
+                      <h2 className="text-[15px] font-bold whitespace-nowrap ">
+                        {cartData?.[0].title}
+                      </h2>
+                      <p className="text-sm font-bold text-black/50 ">
+                        $ {cartData?.[0].price}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex font-bold text-sm py-4 items-center justify-evenly gap-4 w-[120px] bg-[#F1F1F1] ">
+                    <div onClick={handleDecrement} className="text-black/70">
+                      -
+                    </div>
+                    <div className="">{count}</div>
+                    <div onClick={handleIncrement} className="text-black/70">
+                      +
+                    </div>
+                  </div>
+                </div>
+
+                 <div className="flex items-center justify-between ">
+                  <div className="flex gap-2">
+                    <div className="h-[64px] w-[64px] bg-[#F1F1F1] rounded flex items-center justify-center ">
+                      <img
+                        src={cartData?.[0].image}
+                        alt=""
+                        className=" h-[40px] rounded "
+                      />
+                    </div>
+                    <div className="flex flex-col items-start justify-start gap-[2px] w-[76px] overflow-x-auto">
+                      <h2 className="text-[15px] font-bold whitespace-nowrap ">
+                        {cartData?.[0].title}
+                      </h2>
+                      <p className="text-sm font-bold text-black/50 ">
+                        $ {cartData?.[0].price}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex font-bold text-sm py-4 items-center justify-evenly gap-4 w-[120px] bg-[#F1F1F1] ">
+                    <div onClick={handleDecrement} className="text-black/70">
+                      -
+                    </div>
+                    <div className="">{count}</div>
+                    <div onClick={handleIncrement} className="text-black/70">
+                      +
+                    </div>
+                  </div>
+                </div>
+
+                {/* cart content end here */} 
+                {/* work on it later */}
+
               </div>
               <div className="py-4 px-6 flex justify-between items-center">
                 <h2 className="text-[15px] font-bold text-black/50 ">Total</h2>
